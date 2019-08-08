6b0cb6dfcb96b3b4c3b9057a47363b4e68847e5d
Salman Muin Kayser Chishti <13schishti@Gmail.com>
2019-08-08 15:24:49 +0100

    concurrentDictionary, add or update

--- leetCodeStuffPracticingConcurrentDictionary/Program.cs
+++ leetCodeStuffPracticingConcurrentDictionary/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,20 @@ namespace leetCodeStuffPracticingConcurrentDictionary
 {
     class Program
     {
-        static void Main(string[] args)
+        static public void Main()
         {
+            string userinput = Console.ReadLine();
+            ConcurrentDictionary<char, int> mapping = new ConcurrentDictionary<char, int>();
+            foreach (var c in userinput)
+            {
+                mapping.AddOrUpdate(c, 1, (key, oldvalue) => oldvalue + 1);
+            }
+            StringBuilder sb = new StringBuilder();
+            foreach (var c in mapping)
+            {
+                sb.Append(c);
+            }
+            Console.WriteLine(sb.ToString());
         }
     }
 }
